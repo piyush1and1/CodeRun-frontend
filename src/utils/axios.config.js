f721c4ce213f328b64ce411dbfad6f1c8c96342e
piyush1and1 <piyushkumarofficial8096@gmail.com>
2025-11-18 21:05:48 +0530

    added backend

--- src/utils/axios.config.js
+++ src/utils/axios.config.js
@@ -1,13 +1,8 @@
 import axios from 'axios';
 
-// Create a central Axios instance
 const api = axios.create({
-  // The base URL will be proxied by Vite to http://localhost:5000/api
-  baseURL: '/api', 
-  
-  // CRITICAL: This tells Axios to send cookies (like your auth token)
-  // with every request to the backend.
-  withCredentials: true, 
+  baseURL: import.meta.env.VITE_API_URL || '/api',
+  withCredentials: true,
 });
 
 export default api;
